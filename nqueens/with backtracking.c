dc0908558c92969d37b0535b2f83181dedca30a2
Gella Naga Sai Krishna <saikrishg888@gmail.com>
2025-09-14 18:08:44 +0530

    Update with backtracking.c

--- nqueens/with backtracking.c
+++ nqueens/with backtracking.c
@@ -1,104 +1,89 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<assert.h>
-#include<stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
 
-#include<math.h>
 #ifdef _WIN32
 typedef long ssize_t;
 #endif
 
-
-struct node{
+struct node {
     size_t n;
-    size_t *queens;
-    size_t k;//current row
-    ssize_t last;
+    size_t *queens; // queens[row] = column index
+    size_t k;       // current row
+    ssize_t last;   // last tried column in this row
 };
 
-void root(size_t n,struct node*p){
-    p->n=n;
-    p->queens=malloc(n*sizeof(size_t));
+void root(size_t n, struct node *p) {
+    p->n = n;
+    p->queens = malloc(n * sizeof(size_t));
     assert(p->queens);
-    p->k=0;
-    p->last=-1;
+    p->k = 0;
+    p->last = -1;
 }
 
-bool leaf(struct node*p){
-    if(p->k==p->n){return true;}
-    return false;
+bool leaf(struct node *p) {
+    return p->k == p->n;
 }
 
 bool ok(struct node *p) {
-    for (size_t i = 0; i < p->k; i++) {
-        if (p->queens[i] == p->last) return false; // same column
-        if (p->queens[i] + (p->k - i) == (size_t)p->last) return false; // ↘ diagonal
-        if (p->queens[i] - (p->k - i) == (size_t)p->last) return false; // ↗ diagonal
+    size_t row = p->k - 1;     // last placed queen
+    size_t col = p->queens[row];
+    for (size_t i = 0; i < row; i++) {
+        size_t qcol = p->queens[i];
+        if (qcol == col) return false;                            // same column
+        if ((ssize_t)qcol - (ssize_t)i == (ssize_t)col - (ssize_t)row) return false; // ↘ diagonal
+        if ((ssize_t)qcol + (ssize_t)i == (ssize_t)col + (ssize_t)row) return false; // ↗ diagonal
     }
     return true;
 }
 
-bool up(struct node*p){
-    if(p->k==0){return false;}
+bool up(struct node *p) {
+    if (p->k == 0) return false;
     p->k--;
     p->last = p->queens[p->k];
     return true;
 }
 
 bool down(struct node *p) {
-    bool *used = malloc(p->n * sizeof(bool));
-    assert(used);
-
-    for (size_t i = 0; i < p->n; ++i)
-        used[i] = false;
-
-    for (size_t i = 0; i < p->k; ++i)
-        used[p->queens[i]] = true;
-
-    for (size_t i = p->last + 1; i < p->n; ++i)
-        if (!used[i]) {
-            p->queens[p->k] = i;
-            ++(p->k);
-            p->last = -1; // fixed spacing
-            free(used);
+    for (size_t i = p->last + 1; i < p->n; i++) {
+        p->queens[p->k] = i;
+        p->last = i;
+        p->k++;
+        if (ok(p)) {  // check immediately after placing
+            p->last = -1;
             return true;
         }
-
-    free(used);
+        p->k--; // undo if not valid
+    }
     return false;
 }
 
-
-size_t nqueens(size_t n){
+size_t nqueens(size_t n) {
     struct node p;
-    root(n,&p);
-    size_t c = 0;
-    while(true){
-        if(leaf(&p)){
-            if(ok(&p)){c++;} 
-            if(!up(&p)){
-            break;
-        }     }
-        else{
-            if(!ok(&p)){
-                if(!up(&p)){
-                    break;
-                }}
-            else{
-                if(!down(&p)){
-                    if(!up(&p)){
-                        break;}     }
-            }  }
+    root(n, &p);
+    size_t count = 0;
+
+    while (true) {
+        if (leaf(&p)) {
+            count++; // full solution found
+            if (!up(&p)) break;
+        } else {
+            if (!down(&p)) {
+                if (!up(&p)) break;
+            }
+        }
     }
-      free(p.queens);
-    return c;
+
+    free(p.queens);
+    return count;
 }
-int main(){
+
+int main() {
     size_t n;
-    printf("Enter the value of n(number of queens): ");
+    printf("Enter the value of n (number of queens): ");
     scanf("%zu", &n);
     size_t result = nqueens(n);
     printf("Number of solutions for %zu queens: %zu\n", n, result);
     return 0;
-
 }
